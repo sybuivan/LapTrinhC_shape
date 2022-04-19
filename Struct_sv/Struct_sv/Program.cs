47e087a8c807933c13b73c0fd751b68066c7fdbe
sybuivan <sybuivan1429@gmail.com>
2022-04-19 19:09:36 +0700

    update struct sv

---
+++ Struct_sv/Struct_sv/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_sv
+{
+    class Program
+    {
+        struct SinhVien
+        {
+            public int MASO_248;
+            public string HOTEN_248;
+            public double DIEMC_248;
+            public double DIEMJAVA_248;
+            public double DIEMWEB_248;
+            public thuocTinhSv_248 GIOITINH_248;
+        }
+
+        enum thuocTinhSv_248
+        {
+            //0 nu - 1 nam
+            NAM = 0,
+            NU = 1
+        }
+
+        static SinhVien NhapThongTinSinhVien_248(SinhVien SV)
+        {
+            Console.Write(" Ma so: ");
+            SV.MASO_248 = int.Parse(Console.ReadLine());
+            Console.Write(" Ho ten: ");
+            SV.HOTEN_248 = Console.ReadLine();
+            Console.Write(" Nhap Gioi tinh: ");
+            SV.GIOITINH_248 = (Console.ReadLine()) == "0" ? thuocTinhSv_248.NAM : thuocTinhSv_248.NU;
+                
+            Console.Write(" Diem C: ");
+            SV.DIEMC_248 = Double.Parse(Console.ReadLine());
+            Console.Write(" Diem JAVA: ");
+            SV.DIEMJAVA_248 = Double.Parse(Console.ReadLine());
+            Console.Write(" Diem WEB: ");
+            SV.DIEMWEB_248 = Double.Parse(Console.ReadLine());
+            return SV;
+        }
+
+        static void XuatThongTinSinhVien_248(SinhVien SV)
+        {
+            Console.WriteLine(" Ma so: " + SV.MASO_248);
+            Console.WriteLine(" Ho ten: " + SV.HOTEN_248);
+            if(SV.GIOITINH_248 == 0)
+            {
+                Console.WriteLine(" Gioi tinh Nam");
+            } else
+            {
+                Console.WriteLine(" Gioi tinh Nu");
+            }
+            Console.WriteLine(" Diem C: " + SV.DIEMC_248);
+            Console.WriteLine(" Diem JAVA: " + SV.DIEMJAVA_248);
+            Console.WriteLine(" Diem WEB: " + SV.DIEMWEB_248);
+        }
+
+        static double DiemTBSinhVien_248(SinhVien SV)
+        {
+            return (SV.DIEMC_248 + SV.DIEMJAVA_248 + SV.DIEMWEB_248) / 3;
+        }
+
+        static void FindByName_248(SinhVien[] arr_sv_248, String keyword_248, int soLuong_248)
+        {
+            foreach (SinhVien sv in arr_sv_248)
+            {
+                if (String.Compare(sv.HOTEN_248, keyword_248) ==0)
+                {
+                    Console.WriteLine("Tim thay sinh vien: {0} ", sv.HOTEN_248);
+                    Console.WriteLine("Diem tb sinh vien: {0}", DiemTBSinhVien_248(sv));
+                    break;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int soLuong_248, count = 1;
+
+            Console.Write("Nhap bao nhieu sinh vien: ");
+            soLuong_248 = int.Parse(Console.ReadLine());
+
+            SinhVien[] arr_sv_248 = new SinhVien[soLuong_248];
+
+            for(int i_248 = 0; i_248 < soLuong_248; i_248++)
+            {
+                SinhVien sv = new SinhVien();
+                Console.WriteLine("\t Nhap sinh vien: {0}", (i_248));
+                sv = NhapThongTinSinhVien_248(sv);
+                arr_sv_248[i_248] = sv;
+            }
+           
+
+            foreach (SinhVien sv in arr_sv_248)
+            {
+
+                Console.WriteLine("\t Thong tin sinh vien: {0}", (count));
+                XuatThongTinSinhVien_248(sv);
+                count++;
+            }
+
+            string ten_248;
+            Console.WriteLine("\n\t Nhap ten sinh vien can tim:");
+            ten_248 = Console.ReadLine();
+
+            FindByName_248(arr_sv_248, ten_248, soLuong_248);
+            Console.ReadKey();
+        }
+    }
+}
