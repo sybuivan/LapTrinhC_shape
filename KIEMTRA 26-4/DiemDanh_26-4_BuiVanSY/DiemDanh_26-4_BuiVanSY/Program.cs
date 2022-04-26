1ea3e6a1fc1b023446ce5421ad818967e43c22a0
sybuivan <sybuivan1429@gmail.com>
2022-04-26 07:51:58 +0700

    add

--- KIEMTRA 26-4/DiemDanh_26-4_BuiVanSY/DiemDanh_26-4_BuiVanSY/Program.cs
+++ KIEMTRA 26-4/DiemDanh_26-4_BuiVanSY/DiemDanh_26-4_BuiVanSY/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,80 @@ using System.Threading.Tasks;
 
 namespace DiemDanh_26_4_BuiVanSY
 {
-    class Program
+    class ptb2_248
+    {
+        private double a_248, b_248, c248;
+
+        public double A_248 { get => a_248; set => a_248 = value; }
+        public double B_248 { get => b_248; set => b_248 = value; }
+        public double C248 { get => c248; set => c248 = value; }
+
+        public ptb2_248(double a_248, double b_248, double c248)
+        {
+            this.A_248 = a_248;
+            this.B_248 = b_248;
+            this.C248 = c248;
+        }
+
+        public ptb2_248()
+        {
+        }
+
+        public double delta_248(double a_248, double b_248, double c_248)
+        {
+            return a_248 * b_248 - 4 * a_248 * c_248;
+        }
+
+
+
+    public void GiaiHPT(double a_248, double b_248, double c_248)
+    {
+        if (delta_248(a_248, b_248, c_248) > 0)
+        {
+            Console.WriteLine("Phuong trinh co hai nghiem : ");
+            Console.WriteLine("X1 = {0}", ((-b_248 - Math.Sqrt(delta_248(a_248, b_248, c_248)) / 2 * a_248)));
+            Console.WriteLine("X2 = {0}", ((-b_248 + Math.Sqrt(delta_248(a_248, b_248, c_248))) / 2 * a_248));
+
+        }
+        else if (delta_248(a_248, b_248, c_248) == 0)
+        {
+            Console.WriteLine("Phuong trinh co hai nghiem kep nghiem");
+            Console.WriteLine("X1 = X2 {0}", -b_248 / 2 * c_248);
+        }
+        else if (delta_248(a_248, b_248, c_248) < 0)
+        {
+            Console.WriteLine("Phuong trinh vo nghiem");
+        }
+    }
+
+}
+
+    class Animal
+    {
+        public virtual void eat()
+        {
+
+        }
+
+        public virtual void makeSound()
+        {
+
+        }
+    }
+
+class Program
     {
         static void Main(string[] args)
         {
+            double a_248, b_248, c_248;
+            Console.WriteLine("Nhap vao a : ");
+            a_248 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap vao b : ");
+            b_248 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap vao c : ");
+            c_248 = float.Parse(Console.ReadLine());
+            ptb2_248 ptb2 = new ptb2_248();
+            ptb2.GiaiHPT(a_248, b_248, c_248);
         }
     }
 }
