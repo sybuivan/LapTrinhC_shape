19c3f056db18bce626d6b6ede5ccc0e69e745f0f
sybuivan <sy.buivan@ncc.asia>
2022-05-10 07:55:16 +0700

    add sap xep tang giam

--- BaiTapDiemDanh(10_5)/BaiTapDiemDanh(10_5)/Program.cs
+++ BaiTapDiemDanh(10_5)/BaiTapDiemDanh(10_5)/Program.cs
@@ -12,8 +12,15 @@ namespace BaiTapDiemDanh_10_5_
             nhapSoNguyen(a_248, n_248);
             xuatSoNguyen(a_248, n_248);
 
-            Console.Write("So lon nhat: {0}", Max(a_248, n_248));
+            Console.WriteLine("So lon nhat: {0}", Max(a_248, n_248));
             Console.Write("So Nho nhat: {0}", Min(a_248, n_248));
+            Console.WriteLine("Sap xep giam dan:");
+            Sapxepgiamdan(a_248, n_248);
+            xuatSoNguyen(a_248, n_248);
+
+            Console.WriteLine("Sap xep tang dan: ");
+            Sapxeptangdan(a_248,n_248);
+            xuatSoNguyen(a_248, n_248);
 
             Console.Write("So luot xuat hien phan tu nhieu nhat: {0}", countElement(a_248, n_248));
             Console.ReadKey();
@@ -29,7 +36,7 @@ namespace BaiTapDiemDanh_10_5_
 
         static int Max(int[] arr_248, int n)
         {
-            int Max_248 = arr_248[0];
+            int Max_248 = arr_248[1];
             for (int i = 1; i <= n; i++)
             {
                 if (Max_248 < arr_248[i])
@@ -40,7 +47,7 @@ namespace BaiTapDiemDanh_10_5_
 
         static int Min(int[] arr_248, int n_248)
         {
-            int min_248 = arr_248[0];
+            int min_248 = arr_248[1];
             for (int i = 1; i <= n_248; i++)
             {
                 if (arr_248[i] < min_248)
@@ -73,6 +80,39 @@ namespace BaiTapDiemDanh_10_5_
             return max_248;
         }
 
+        static void Sapxepgiamdan(int[] a_248, int n)
+        {
+            int i, j, tmp;
+            for (i = 1; i <= n; i++)
+            {
+                for (j = i + 1; j <= n; j++)
+                {
+                    if (a_248[i] < a_248[j])
+                    {
+                        tmp = a_248[i];
+                        a_248[i] = a_248[j];
+                        a_248[j] = tmp;
+                    }
+                }
+            }
+        }
+
+        static void Sapxeptangdan(int[] a_248, int n)
+        {
+            int i, j, tmp;
+            for (i = 1; i <= n; i++)
+            {
+                for (j = i + 1; j <= n; j++)
+                {
+                    if (a_248[i] > a_248[j])
+                    {
+                        tmp = a_248[i];
+                        a_248[i] = a_248[j];
+                        a_248[j] = tmp;
+                    }
+                }
+            }
+        }
         static void xuatSoNguyen(int[] a_248, int n_248)
         {
             Console.Write("Hien thi mang:");
