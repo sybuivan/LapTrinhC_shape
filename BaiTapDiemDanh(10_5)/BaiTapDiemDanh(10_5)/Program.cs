b0324799f810f1e388c39c794cc3f20f01be97d1
sybuivan <sy.buivan@ncc.asia>
2022-05-10 07:44:24 +0700

    update

--- BaiTapDiemDanh(10_5)/BaiTapDiemDanh(10_5)/Program.cs
+++ BaiTapDiemDanh(10_5)/BaiTapDiemDanh(10_5)/Program.cs
@@ -12,21 +12,71 @@ namespace BaiTapDiemDanh_10_5_
             nhapSoNguyen(a_248, n_248);
             xuatSoNguyen(a_248, n_248);
 
+            Console.Write("So lon nhat: {0}", Max(a_248, n_248));
+            Console.Write("So Nho nhat: {0}", Min(a_248, n_248));
+
+            Console.Write("So luot xuat hien phan tu nhieu nhat: {0}", countElement(a_248, n_248));
             Console.ReadKey();
         }
         static void nhapSoNguyen(int[] a_248, int n_248)
         {
-            for (int i = 0; i < n_248; i++)
+            for (int i = 1; i <= n_248; i++)
             {
                 Console.Write("a[" + i + "]=");
                 a_248[i] = int.Parse(Console.ReadLine());
             }
         }
 
+        static int Max(int[] arr_248, int n)
+        {
+            int Max_248 = arr_248[0];
+            for (int i = 1; i <= n; i++)
+            {
+                if (Max_248 < arr_248[i])
+                    Max_248 = arr_248[i];
+            }
+            return Max_248;
+        }
+
+        static int Min(int[] arr_248, int n_248)
+        {
+            int min_248 = arr_248[0];
+            for (int i = 1; i <= n_248; i++)
+            {
+                if (arr_248[i] < min_248)
+                    min_248 = arr_248[i];
+            }
+            return min_248;
+        }
+
+        static int countElement(int[] a_248, int n_248)
+        {
+            int max_248 = 0;
+            int dem_248 = 1;
+            for (int i = 1; i <= n_248; i++)
+            {
+                if (a_248[i] == a_248[i + 1])
+                {
+                    dem_248++;
+                    if (dem_248 > max_248)
+                    {
+                        max_248 = dem_248;
+                    }
+
+                }
+                else
+                {
+
+                    dem_248 = 1;
+                }
+            }
+            return max_248;
+        }
+
         static void xuatSoNguyen(int[] a_248, int n_248)
         {
             Console.Write("Hien thi mang:");
-            for (int i = 0; i < n_248; i++)
+            for (int i = 1; i <= n_248; i++)
                 Console.Write(a_248[i] + " ");
         }
 
